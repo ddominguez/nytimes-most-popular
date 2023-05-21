633abedf71ef0a521179700b240e42f51c46e438
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-05-20 22:43:51 -0400

    more tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,45 @@
-from app import app
+import pytest
+import requests
+
+from app import app, fetch_most_popular, LIST_TYPES
 
 
-def test_index():
+# https://docs.pytest.org/en/7.3.x/how-to/monkeypatch.html#monkeypatching-returned-objects-building-mock-classes
+class MockResponse:
+    @staticmethod
+    def json():
+        return {"results": []}
+
+    @staticmethod
+    def raise_for_status():
+        pass
+
+
+@pytest.fixture
+def mock_most_popular(monkeypatch):
+    def mock_get(*args, **kwargs):
+        return MockResponse()
+
+    monkeypatch.setattr(requests, "get", mock_get)
+    monkeypatch.setitem(app.config, "NYT_API_KEY", "fake")
+
+
+def test_fetch_most_popular(mock_most_popular):
+    results = fetch_most_popular("emailed")
+    assert "results" in results
+
+
+def test_index_request():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
 
 
-def test_render_most_popular_not_found():
+# @pytest.mark.skip()
+@pytest.mark.parametrize(
+    "test_param,expected", [(t, 200) for t in LIST_TYPES] + [("bad", 404)]
+)
+def test_render_most_popular_request(mock_most_popular, test_param, expected):
     client = app.test_client()
-    response = client.get("/most-popular/invalid")
-    assert response.status_code == 404
+    response = client.get(f"/most-popular/{test_param}")
+    assert response.status_code == expected
