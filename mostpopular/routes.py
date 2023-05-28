93c31159db4b54012beb99e75cc094c6af0969c3
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-05-28 17:45:32 -0400

    Cache request repsonse

--- mostpopular/routes.py
+++ mostpopular/routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, abort, current_app, render_template
 
 from mostpopular.constants import LIST_TYPES
-from mostpopular.extensions import cache
 from mostpopular.utils import fetch_most_popular
 
 nyt_app = Blueprint("nyt_app", __name__, template_folder="templates")
@@ -13,11 +12,10 @@ def index():
 
 
 @nyt_app.get("/most-popular/<list_type>")
-@cache.cached()
 def render_most_popular(list_type=None):
     if list_type not in LIST_TYPES:
         abort(404)
-    data = fetch_most_popular(current_app.config.get('NYT_API_KEY'), list_type)
+    data = fetch_most_popular(current_app.config.get("NYT_API_KEY"), list_type)
     context = {
         "list_types": LIST_TYPES,
         "list_type": list_type,
--- mostpopular/utils.py
+++ mostpopular/utils.py
@@ -1,9 +1,19 @@
+from typing import Any
+
 import requests
 
 from mostpopular.constants import API_URI
+from mostpopular.extensions import cache
 
-def fetch_most_popular(api_key, list_type, period=1):
+
+def fetch_most_popular(api_key, list_type, period=1) -> Any:
+    cache_key = f"{list_type}:{period}"
+    if cache.has(cache_key):
+        return cache.get(cache_key)
+
     url = f"{API_URI}{list_type}/{period}.json?api-key={api_key}"
     resp = requests.get(url)
     resp.raise_for_status()
-    return resp.json()
+    data = resp.json()
+    cache.set(cache_key, data)
+    return data
